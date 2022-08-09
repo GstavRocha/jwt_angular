87a412ab797918fbead9e2724223d3f72ce85bb7
GstavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-08-09 19:49:08 -0300

    api-pronta

---
+++ API/index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+const SECRET = "mysecret"
+app.use(bodyParser.json());
+
+app.get('/',(req,res,next) => {
+    res.json({message: "Tudo está OK"})
+})
+const verifyJWT = (req, res, next) =>{
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+app.get('/clientes',(req, res, next) => {
+    console.log( "Retornou cliente ");
+    res.json([{id:1,nome:'Aluno Exemplar'}]);
+})
+app.post('/login',( req, res) =>{
+    if(req.body.user === 'Gustavo' && req.body.password === '123'){
+        const id = 1;
+        let token = jwt.sign({id},SECRET, {expiresIn: 300});
+        return res.json({ auth: true, token: token});
+    }
+    res.status(500).json({message: 'Login Inválido'});
+})
+
+app.post('/logout',function (req, res){
+    res.json({auth: false, token: null});
+})
+
+const server = http.createServer(app);
+server.listen(3000);
+console.log("Servidor escutando porta 3000")
