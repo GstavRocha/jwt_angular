1372881032220e5033dcd5e46605ecdf670992c6
GstavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-08-09 22:21:49 -0300

    angular]

--- projeto-angular/src/app/app.module.ts
+++ projeto-angular/src/app/app.module.ts
@@ -6,10 +6,14 @@ import {MenuItem} from 'primeng/api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
