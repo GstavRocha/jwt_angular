e67cb0cba7d14b6d5135dc9af7e0ced451869375
GstavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-08-19 06:26:37 -0300

    corrigindo

--- projeto-angular/src/app/users.service.ts
+++ projeto-angular/src/app/users.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
-const API_URL = 'http://localhost:3000/clientes';
+import {stringifyTask} from "@angular/compiler-cli/ngcc/src/execution/tasks/utils";
+const API_URL = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
 
-  constructor(private http: HttpClient) {}
-  getPublicContent(): Observable<any>{
-    return this.http.get(API_URL + 'all', {responseType: 'text'});
+export class UsersService {
+  localhost: string;
+  localHostCliente: string;
+  token: string;
+  constructor(private http: HttpClient) {
+    this.token = '';
+    this.localhost = "http://localhost:3000"
+    this.localHostCliente = "http://localhost:3000/clientes"
   }
+  getClientes(): Observable<any>{
+    this.token = localStorage.getItem('access-token')!;
+    let httpOtptions = {
+      headers: new Headers({
+        'Content-Type' : 'application/json',
+        'x-access-token': this.token
+      })
+    }
+    return this.http.get<any>(`${API_URL}/clientes`,{
+      httpOtptions
+    })
+  }
+
 }
